986ea94f55bbba6186eb2354ffa450a2f06eae3b
KutuaZ <renatoortiz2003@gmail.com>
2025-11-10 01:15:44 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'OptiEatGo',
+  webDir: 'www'
+};
+
+export default config;
