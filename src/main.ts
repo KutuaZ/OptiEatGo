6818fd456815544ae09c07f83ff5d3dd3fbf5950
KutuaZ <renatoortiz2003@gmail.com>
2025-12-01 12:59:38 -0300

    paginas api

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { provideLottieOptions } from 'ngx-lottie';
-import player from 'lottie-web';
+import player from 'lottie-web/build/player/lottie_light';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
